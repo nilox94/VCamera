0d85eb2ddcae25179353c8021c623c59eb5762c9
Danilo Gómez Gómez <danilogomez3.14@gmail.com>
2018-07-04 12:50:54 -0400

    Add bottle app to choose the camera

---
+++ camera-select-app.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from bottle import Bottle, template, static_file
+
+app = Bottle()
+
+@app.route('/')
+def index():
+    return template('camera-select.html', template_lookup=['templates'])
+
+@app.route('/static/<res:path>')
+def static(res):
+    return static_file(res, root='static')
+
+
+if __name__ == '__main__':
+    app.run()
