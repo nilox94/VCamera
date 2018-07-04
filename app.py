8971297dc51c37599b54ce7b7443765c24a0e65c
Marcelo Fornet <mfornet94@gmail.com>
2018-07-04 16:23:33 -0500

    Memento Camera

--- app.py
+++ app.py
@@ -17,7 +17,6 @@ class MyVCamera(VCamera):
 
         self.prev_frame = None
 
-
     def transform(self, bgr_frame):
         if self.prev_frame is None:
             self.prev_frame = bgr_frame
@@ -28,7 +27,30 @@ class MyVCamera(VCamera):
         return frame.astype(np.uint8)
 
 
+class MementoCamera(VCamera):
+    FRAMES = 100
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.new_memory = []
+        self.current_memory = []
+
+
+    def transform(self, bgr_frame):
+        self.new_memory.append(bgr_frame)
+
+        if len(self.new_memory) == MementoCamera.FRAMES:
+            self.current_memory = self.new_memory
+            self.new_memory = []
+
+        if len(self.current_memory) == 0:
+            return bgr_frame
+        else:
+            return self.current_memory[-1 - len(self.new_memory)]
+
+
 if __name__ == '__main__':
-    vcam = MyVCamera(flip=True)
+    vcam = MementoCamera(flip=True)
     vcam.start()
     vcam.join()
